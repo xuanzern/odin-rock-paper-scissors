2cb11da7cc9d1502394173ca5d8f051c31941d2e
xz <xuanzernl@gmail.com>
2024-05-02 23:28:35 +0800

    function for playing a round

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     const randomNum = Math.floor((Math.random()*3));  //random number (0-2)
@@ -6,8 +8,29 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    const choice = prompt("Enter 1 for rock, 2 for paper, 3 for scissors")-1;
+    const choice = prompt("Enter 1 for rock, 2 for paper, 3 for scissors")-1; //prompt player for choice
     return choices[choice];
 }
 
+function playRound(humanChoice, computerChoice){
+    //human win
+    if (humanChoice == choices[0] && computerChoice == choices[2] ||
+        humanChoice == choices[1] && computerChoice == choices[0]  ||
+        humanChoice == choices[2] && computerChoice == choices[1]){
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        }
+    //computer win
+    else if (computerChoice == choices[0] && humanChoice == choices[2] ||
+        computerChoice == choices[1] && humanChoice == choices[0]  ||
+        computerChoice == choices[2] && humanChoice == choices[1]){
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    //tie
+    else{
+        console.log("It's a tie!")
+    }
+}
 
+playRound(getHumanChoice(), getComputerChoice())
