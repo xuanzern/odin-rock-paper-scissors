e25ab03f9b2d45d5b5f5cc1fd90374f4e4084fc6
xz <xuanzernl@gmail.com>
2024-05-02 23:09:16 +0800

    function for computer choice (returns r, p, or s)

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor((Math.random()*3))+1;
+    return choices[randomNum];
+}
+
+console.log(getComputerChoice())
